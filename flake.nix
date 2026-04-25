bfcd56e64cda19e82b80e56d27e5c8cdfa814389
naok-000 <naotaro.kobayashi.000@gmail.com>
2026-04-25 23:10:11 +0900

    build: Add Nix flake and direnv for development environment setup

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "MikanOS development environment on macOS";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+      llvm = pkgs.llvmPackages_14;
+    in {
+      devShells.default = pkgs.mkShell {
+        packages = with pkgs; [
+          git
+          gnumake
+          nasm
+          qemu
+          acpica-tools
+          unzip
+          curl
+          wget
+          python3
+          mtools
+
+          llvm.clang
+          llvm.lld
+          llvm.llvm
+        ];
+      };
+    });
+}
